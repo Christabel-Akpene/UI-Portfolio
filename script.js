2020c0cf31b8176ccb0d09a8b662758a479b14cb
Christabel-Akpene <dc.akpene@gmail.com>
2025-04-29 03:09:34 +0000

    Add rest of pages

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ var typed = new Typed(".dev", {
     backDelay: 1000,
     loop: true
 });
+
+let valueDisplay = document.querySelectorAll(".num");
+let interval = 4000;
+
+valueDisplay.forEach((value) => {
+    let startValue = 0;
+    let endValue = parseInt(value.getAttribute("data-val"));
+    let duration = Math.floor(interval / endValue);
+    let counter = setInterval(function(){
+        startValue += 1;
+        value.textContent = startValue;
+        if (startValue == endValue){
+            clearInterval(counter)
+        }
+    }, duration);
+})
